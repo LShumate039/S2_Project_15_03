ead7790ccd582ce49958e5ac7cd8c1f3fb56ea7f
Unknown <LShumate039@west-mec.org>
2019-04-24 18:25:51 -0700

    update 4/24
    made if statements

--- mas_register.js
+++ mas_register.js
@@ -33,7 +33,7 @@ window.addEventListener("load", function () {
 
       document.getElementById("fnBox").onblur = calcCart;
 
-      document.getElementById("InBox").onblur = calcCart;
+      document.getElementById("lnBox").onblur = calcCart;
 
       document.getElementById("groupBox").onblur = calcCart;
 
@@ -41,9 +41,54 @@ window.addEventListener("load", function () {
 
       document.getElementById("phoneBox").onblur = calcCart;
 
-      document.getElementById(banquetBox).onblur = calcCart;
+      document.getElementById("banquetBox").onblur = calcCart;
 
       document.getElementById("sessionBox").onchange = calcCart;
 
       document.getElementById("mediaCB").onclick = calcCart;
 });
+//displaying "select a session package" on the browser
+function sessionTest() {
+      var confSession = document.getElementById("sessionBox");
+      if (confSession = -1) {
+            confSession.setCustomValidity("Select a Session Package");
+      } else {
+            confSession.setCustomValidity("")
+      }
+}
+
+
+function calcCart() {
+      sessionStorage.confName = document.forms.regForm.elements.fnBox.value + " " + document.forms.regForm.elements.lnBox.value;
+
+      sessionStorage.confGroup = document.forms.regForm.elements.group.value;
+      sessionStorage.confMail = document.forms.regForm.elements.email.value;
+      sessionStorage.confPhone = document.forms.regForm.elements.phoneNumber.value;
+      sessionStorage.confBanquet = document.forms.regForm.elements.banquetGuest.value;
+      //The cost of the banquet is $55 per guest
+      sessionStorage.confBanquetCost = sessionStorage.confBanquet * 55;
+
+      var selectedIndex = document.getElementById("sessionBox").selectedIndex;
+
+      if (sessionBox.selectedIndex !== -1) {
+
+            sessionStorage.confSession = document.forms.regForm.elements.sessionBox[selectedIndex].text;
+
+            sessionStorage.confSessionCost = document.forms.regForm.elements.sessionBox[selectedIndex].value;
+      } else {
+            sessionStorage.confSession = "";
+            sessionStorage.confSessionCost = 0;
+      }
+
+      if (document.getElementById("mediaCB").onclick) {
+            sessionStorage.confPack = "Yes";
+            sessionStorage.confPackCost = 115;
+      } else {
+            sessionStorage.confPack = "No";
+            sessionStorage.confPackCost = 0;
+      }
+
+      sessionStorage.confTotal = parseFloat(sessionStorage.confBanquetCost) + parseFloat(sessionStorage.confPackCost) + parseFloat(sessionStorage.confSessionCost)
+
+      writeSessionValues();
+}
